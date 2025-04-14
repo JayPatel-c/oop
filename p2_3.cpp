894290b55ba9e6abe4c3f8d43e398dae3035cb9b
JayPatel-c <jayyspatel@gmail.com>
2025-04-14 22:22:10 +0530

    Update p2_3.cpp

--- p2_3.cpp
+++ p2_3.cpp
@@ -11,77 +11,81 @@ class Account
 
     void CreateAccount()
     {
+        cout << "--------------------------" << endl;
         cout<<"Enter Holder Name : ";
         cin>>HolderName;
         cout<<"Enter Account Number : ";
         cin>>AccountNo;
     b1:
-        cout<<"Enter current Balance : ";
+        cout<<"Enter your Balance : ";
         cin>>CurrentBalance;
+        cout << "--------------------------" << endl;
         if(CurrentBalance<1000)
         {
             cout<<"Minimum required balance is 1000..."<<endl;
+            cout << "--------------------------" << endl;
             goto b1;
         }
     }
-
-    void DepositMoney(int CheckAccountNo)
+    int getNO()
     {
-        if(CheckAccountNo==AccountNo)
-        {
+        return AccountNo;
+    }
+
+    void DepositMoney()
+    {
         double NewAmount;
+        cout << "--------------------------" << endl;
         cout<<"Enter your amount that you want to deposit : ";
         cin>>NewAmount;
-        CurrentBalance=CurrentBalance+NewAmount;
+        CurrentBalance+=NewAmount;
         cout<<"Your new balance is : "<<CurrentBalance<<endl;
-        }
-        else
-        {
-            cout<<"Enter valide account number."<<endl;
-        }
+        cout << "--------------------------" << endl;
     }
 
 
-    double WithdrawMoney(int CheckAccountNo)
+    void WithdrawMoney()
     {
-        if(CheckAccountNo==AccountNo)
-        {
         double WithdrawAmount;
+        cout << "--------------------------" << endl;
         cout<<"Enter your amount that you want to withdraw : ";
         cin>>WithdrawAmount;
-        CurrentBalance=CurrentBalance-WithdrawAmount;
-        cout<<"Your new balance is : "<<CurrentBalance<<endl;
-        }
-        else
-        {
-            cout<<"Enter valide account number."<<endl;
-        }
-        return CurrentBalance;
+
+                if( WithdrawAmount > CurrentBalance )
+                {
+                    cout<<"Account has insufficient balance...";
+                    cout << "--------------------------" << endl;
+                }
+                else
+                {
+                   CurrentBalance=CurrentBalance-WithdrawAmount;
+                   cout<<"New Balance : "<<CurrentBalance<<endl;
+                   cout << "--------------------------" << endl;
+                }
     }
 
-    void DisplayDetails(int CheckAccountNo)
+    void DisplayDetails()
     {
-        if(CheckAccountNo==AccountNo)
-        {
+        cout << "--------------------------" << endl;   
         cout<<"Holder Name : "<<HolderName<<endl;
-        cout<<"number : "<<AccountNo<<endl;
+        cout<<"Number : "<<AccountNo<<endl;
         cout<<"CurrentBalance : "<<CurrentBalance<<endl;
+        cout << "--------------------------" << endl;
         }
-        else
-        {
-            cout<<"Enter valide account number."<<endl;
-        }
-    }
 };
 int main()
 {
-    Account A;
+    Account A[20];
+    int NumAcc=0;
     int choice;
+    int CheckAccountNo;
+    bool flag=false;
    next:
     cout<<"Enter '1' for create Account."<<endl;
     cout<<"Enter '2' for deposit money."<<endl;
     cout<<"Enter '3' for withdraw money."<<endl;
-     cout<<"Enter '4' for Display Details."<<endl;
+    cout<<"Enter '4' for Display Details."<<endl;
+    cout<<"Enter '5' for Exit."<<endl;
     cout<<"Enter your choice : ";
     cin>>choice;
 
@@ -89,34 +93,78 @@ int main()
     {
         case 1:
         {
-            A.CreateAccount();
+            A[NumAcc].CreateAccount();
+            NumAcc++;
             break;
         }
         case 2:
         {
-            int CheckAccountNo;
-            cout<<"Enter account number : ";
-            cin>>CheckAccountNo;
-            A.DepositMoney(CheckAccountNo);
+            cout << "Enter Account no for check : ";
+            cin >> CheckAccountNo;
+            for (int i = 0; i < NumAcc; i++)
+            {
+                if(A[i].getNO() == CheckAccountNo)
+                {
+                    flag=true;
+                    A[i].DepositMoney();
+                    break;
+                }
+            }
+            if(!flag)
+            { 
+                cout << "Enter valid ID." << endl;
+            }
             break;
         }
-         case 3:
+        
+        case 3:
         {
-            int CheckAccountNo;
-            cout<<"Enter account number : ";
-            cin>>CheckAccountNo;
-            A.WithdrawMoney(CheckAccountNo);
-            break;
+            cout << "Enter product ID for check : ";
+            cin >> CheckAccountNo;
+            for (int i = 0; i < NumAcc; i++)
+            {
+                if(A[i].getNO() == CheckAccountNo)
+                {
+                    flag = true;
+                    A[i].WithdrawMoney();
+                    break;
+                }
+                
+            }
+            if(!flag)
+            { 
+                cout << "Enter valid ID." << endl;
+            }
+            break;  
         }
+
         case 4:
         {
-            int CheckAccountNo;
-            cout<<"Enter account number : ";
-            cin>>CheckAccountNo;
-            A.DisplayDetails(CheckAccountNo);
+            cout << "Enter product ID for check : ";
+            cin >> CheckAccountNo;
+            for (int i = 0; i < NumAcc; i++)
+            {
+                if(A[i].getNO() == CheckAccountNo)
+                {
+                    flag = true;
+                    A[i].DisplayDetails();
+                    break;
+                }
+                
+            }
+            if(!flag)
+            { 
+                cout << "Enter valid ID." << endl;
+            }
+            break;  
         }
+        case 5:
+        {
+            cout<<"Thank you, Exit.";
+            return 0;
+        }
+        default:
+            cout << "Invalid choice, try again." << endl;
     }
     goto next;
-
-    return 0;
 }
